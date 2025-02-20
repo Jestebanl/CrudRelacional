8628489ed2b8c4c48cde9ffe6803491d623c7bac
Juan <jestebanl2004@gmail.com>
2025-02-20 19:46:00 +0100

    Creacion del proyecto

---
+++ src/main/java/nebrija/crudrelacional/CrudRelacionalApplication.java
@@ -0,0 +1,13 @@
+package nebrija.crudrelacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRelacionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRelacionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nebrija/crudrelacional/CrudRelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package nebrija.crudrelacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRelacionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
